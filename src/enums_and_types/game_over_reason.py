6316a0b79f39025ddc7944aa4b289efec5b3bbab
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-11-24 21:00:19 +1300

    cleared enums

--- src/enums_and_types/game_over_reason.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class GameOverReason(Enum):
-    HEALTH = 0
-    OUT_OF_TIME = 1
-    BURIED_TOTEM = 2
--- src/enums_and_types/types.py
+++
@@ -1,4 +0,0 @@
-type Position = tuple[int, int]
-"""Position of the player in the form of [x, y].
-Positive x is East, Positive y is North
-"""
